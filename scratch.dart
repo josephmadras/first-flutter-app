b358c0c2dd140e1912675b9e8af43f39c62df2f7
Joseph Madras <josephkabemba77@gmail.com>
2021-12-05 19:54:59 +0100

    refactor: study variables and functions declaration in dart

--- scratch.dart
+++ scratch.dart
@@ -1,6 +1,32 @@
 void main(){
  // var name= "Joseph";
-  dynamic name= "Madras";
-  name=12;
-  print(name);
+  // dynamic name= "Madras";
+  // String name="Madras";
+  //final String name="Madras";
+  //const String name="Madras";
+  //const number =18;
+
+  final String name= "Madras";
+  int age = 35;
+  var hobbies = <String>["coding", "biking", "reading"];
+  //print("My name is $name and I'm $age years old");
+  //print(hobbies);
+  //print(add(45, 55));
+
+  greet();
+  greet(name: "First Lady");
+
+  print("15/4 = ${divide(numerator: 15, denominator: 4)}");
+}
+
+int add(int number1, int number2){
+  return number1 + number2;
+}
+
+double divide({double numerator = 1.0, double denominator = 1.0}){
+  return numerator/denominator;
+}
+
+void greet({String name ="user"}){
+  print("Hello $name");
 }
