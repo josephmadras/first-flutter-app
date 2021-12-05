691e154d9c6a0dd6062b4c1e604dbba125f9e971
Joseph Madras <josephkabemba77@gmail.com>
2021-12-05 12:50:51 +0100

    style: add home icon to sidebar menu

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,30 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: Row(
             children: [
-              Icon(Icons.home),
               Container(
-                child: Text("Joseph Madras"),
+                width: 42.0,
+                  height: 42.0,
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(14.0),
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Color(0xFF00AEFF),
+                        Color(0xFF0076FF),
+                      ]
+                    )
+                  ),
+                  child: Icon(Icons.home, color: Colors.white,)
+              ),
+              SizedBox(width: 12),
+              Container(
+                child: Text("Home", style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w800,
+                  color: Color(0xFF242629),
+                ),),
               ),
             ],
           ),
